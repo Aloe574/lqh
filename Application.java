4560f077faf6d69b177f1cd8fdc28897dd28eee1
Aloe574 <89061231+Aloe574@users.noreply.github.com>
2021-08-22 11:49:14 +0800

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,9 @@
+package ATM;
+
+public class Application {
+    public static void main(String[] args) {
+        //实例化Login类的对象，调用初始化界面的方法
+        Login login =new Login();
+        login.initUI();
+    }
+}
